525ec3c696297a0529759b5ffd3b7fd83b5bcd2b
yeison-bf <yeison.barrios.funieles@gmail.com>
2025-03-15 11:14:34 -0500

    update data deploy

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-// Definir __dirname correctamente en ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src', // Usa una ruta absoluta para evitar problemas
-    },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          icons: ['react-icons']
+        }
+      }
+    }
   },
-});
+  css: {
+    postcss: './postcss.config.js',
+  }
+})
