7702ba00837d14a238432cb683bed52693c443c4
yeison-bf <yeison.barrios.funieles@gmail.com>
2025-03-15 10:34:27 -0500

    update package

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-nesting': {}, // Plugin para anidación de CSS
-    '@tailwindcss/postcss': {}, // Procesa Tailwind CSS
-    autoprefixer: {}, // Agrega prefijos para compatibilidad con navegadores
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
